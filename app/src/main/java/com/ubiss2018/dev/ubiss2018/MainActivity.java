ddae17b7856c38129cdf040b63e3b5232761845e
Mikko Yliniemi <mikkoyli@paju.oulu.fi>
2018-06-07 10:45:23 +0300

    two new activities

--- app/src/main/java/com/ubiss2018/dev/ubiss2018/MainActivity.java
+++ app/src/main/java/com/ubiss2018/dev/ubiss2018/MainActivity.java
@@ -1,56 +1,55 @@
 package com.ubiss2018.dev.ubiss2018;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.View;
 
 import com.aware.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private ImageView imageView;
+    private String fullScreenInd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        button = (Button) findViewById(R.id.meditate_button);
 
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
 
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        Questionnaire.class);
+                startActivity(myIntent);
+            }
+        });
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         /*Intent aware = new Intent(this, Aware.class);
         startService(aware);
         Aware.startScreen(this);*/
 
-        Intent aware = new Intent(this, Aware.class);
+        //Intent aware = new Intent(this, Aware.class);
 
-        StartService(aware);
+
+
+        //StartService(aware);
         Aware.startScreen(this);
 
        /* Screen.setSensorObserver(new Screen.AWARESensorObserver() {
---
+++ app/src/main/java/com/ubiss2018/dev/ubiss2018/Questionnaire.java
@@ -0,0 +1,16 @@
+package com.ubiss2018.dev.ubiss2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+
+public class Questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+
+    }
+}
